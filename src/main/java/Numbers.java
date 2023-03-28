a553a5d7d7a4f5184dc052b526b0efc581e3357b
lmacbride <L00177577@atu.ie>
2023-03-28 19:08:23 +0100

    added java class

---
+++ src/main/java/Numbers.java
@@ -0,0 +1,31 @@
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+        int num2 = 5;
+        int num3 = 7;
+
+        // addition
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+        // subtraction
+        int diff = num1 - num2;
+        System.out.println("Difference between " + num1 + " and " + num2 + " is " + diff);
+
+        // multiplication
+        int product = num1 * num2;
+        System.out.println("Product of " + num1 + " and " + num2 + " is " + product);
+
+        // division
+        int quotient = num1 / num2;
+        System.out.println("Quotient of " + num1 + " and " + num2 + " is " + quotient);
+
+        // modulus
+        int remainder = num1 % num2;
+        System.out.println("Remainder of " + num1 + " and " + num2 + " is " + remainder);
+	}
+
+}
